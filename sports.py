da63e03e2dfbf28dfe8fa592134c99b9fd3095c1
Taylor Cooke <47223694+tlcpack@users.noreply.github.com>
2020-08-05 15:09:26 -0400

    added code to get some team season data

--- sports.py
+++ sports.py
@@ -1,4 +1,6 @@
 from sportsreference.mlb.roster import Roster, Player
+from sportsreference.mlb.teams import Teams
+# from sportsreference.ncaaf.teams import Teams
 
 summary_list = []
 
@@ -10,3 +12,17 @@ for x in range(1990, 1995):
         summary_list.append((pitcher.name, season, pitcher.position, pitcher(season).games, pitcher(season).era, pitcher(season).wins, pitcher(season).whip))
     
 print(summary_list)
+
+# for x in range(2002, 2005):
+#   teams = Teams(x)
+
+#   for team in teams:
+#     if team.abbreviation == 'NORTH-CAROLINA-STATE':
+#       print(str(x) + ' - ' + team.abbreviation + " - " + str(team.wins) + " Rush yards: " + str(team.rush_yards))
+
+for x in range(1992, 2005):
+  teams = Teams(x)
+
+  for team in teams:
+    if team.abbreviation == 'ATL':
+      print(str(x) + ' - ' + team.abbreviation + " - " + str(team.wins) + " ERA: " + str(team.earned_runs_against) + " Pythag - " + team.pythagorean_win_loss)
